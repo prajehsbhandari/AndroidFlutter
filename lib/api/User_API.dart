2100d6219267733e185df01cb53997b58c6f3d32
prajehsbhandari <prajehsbhandari@gmail.com>
2022-06-10 15:56:21 +0545

    UserAPI changes

--- lib/api/User_API.dart
+++ lib/api/User_API.dart
@@ -14,7 +14,7 @@ class User_API {
         url,
         data: user.toJson(),
       );
-      if (response = await dio.post(url, data: user.toJson(),
+      if (response = await dio.post(url, data: user.toJso(),
       );
       )
     }
