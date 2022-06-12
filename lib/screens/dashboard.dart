8da90a594fdc3e8471be9a89ca7a88d0a8c169cd
prajehsbhandari <prajehsbhandari@gmail.com>
2022-06-12 19:45:30 +0545

    Navbar changes

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -11,24 +11,6 @@ class _DashBoardState extends State<DashBoard> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Settings',
-      style: optionStyle,
-    ),
-  ];
 
   void _onItemTapped(int index) {
     setState(() {
